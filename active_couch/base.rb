14551a80ba20c9628178cea6a28e6c28e88b723a
Arun Thampi <arun.thampi@gmail.com>
2007-12-03 06:34:33 +0000

    Added has and has_many class methods to ActiveCouch::Base

--- active_couch/base.rb
+++ active_couch/base.rb
@@ -1,4 +1,50 @@
 module ActiveCouch
-  class Base
+  class ActiveCouchError < StandardError
   end
-end
+  
+  class IllegalArgumentError < ActiveCouchError
+  end
+  
+  class Base
+    class << self # Class methods
+      def define_instance_variable(var_name, default_value)
+        unless var_name.is_a?(Symbol) || var_name.is_a?(String)
+          raise IllegalArgumentError, "#{sym.inspect} is neither a String nor a Symbol"
+        end
+
+        class_eval <<-eval
+          def #{var_name}; @#{var_name} ||= #{default_value}; end
+          def #{var_name}=(val); @#{var_name} = val; end
+        eval
+      end
+      
+      def has(*args)
+        # has can contain a series of symbols each of which will correspond to a text node
+        # in the ActiveCouch::Base object
+        # For e.g. has_one :name, :age, :sex, :location will define the instance variables 
+        # @name, @age, @sex, @location with the default value of an empty string
+        default_value = ""
+        
+        if args.last.is_a?(Hash)
+          options = args.pop
+          default_value = options[:with_default_value] if options.has_key?(:with_default_value)
+        end
+        args.each {|sym| define_instance_variable(sym, default_value) }
+      end
+      
+      def has_many(*args)
+        # has_many can contain a series of symbols each of which will correspond to a text node
+        # in the ActiveCouch::Base object
+        # For e.g. has_many :airports will define the instance variable
+        # @airports with the default value of an empty array
+        default_value = []
+        
+        if args.last.is_a?(Hash)
+          options = args.pop
+          default_value = options[:with_default_value] if options.has_key?(:with_default_value)
+        end
+        args.each {|sym| define_instance_variable(sym, default_value) }
+      end
+    end # end Class methods
+  end # end Base
+end # end ActiveCouch
