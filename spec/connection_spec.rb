d65ae3cb36926bc5611d7846937f1a1aeee077b5
Cheah Chu Yeow <chuyeow@gmail.com>
2008-01-18 06:09:24 +0000

    Clean up connection_spec.rb a little.

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -1,20 +1,9 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-def initialize_connection(spec = nil)
-  begin
-  connection = ActiveCouch::Connection.new(spec)
-  rescue
-  error = $!.to_s
-  end
-  return connection, error
-end
-
 describe ActiveCouch::Connection do
-  before(:each) do
-    @connection, @error = initialize_connection('http://192.168.0.150:7777')
-  end
-  
   it "should set the site for the connection object and there must be no error" do
+    @connection = ActiveCouch::Connection.new('http://192.168.0.150:7777')
+
     @connection.site.host.should == "192.168.0.150"
     @connection.site.port.should == 7777
     @error.should == nil
@@ -22,24 +11,18 @@ describe ActiveCouch::Connection do
 end
 
 describe "An ActiveCouch::Connection object instantiated with no site specified" do
-  before(:each) do
-    @connection, @error = initialize_connection()
-  end
-  
-  it "should raise an error and return nil" do
-    @connection.should == nil
-    @error.should == "Missing site URI"
+  it "should raise an ArgumentError and return nil" do
+    lambda {
+      @connection = ActiveCouch::Connection.new(nil)
+    }.should raise_error(ArgumentError, 'Missing site URI')
   end
 end
 
 describe "An ActiveCouch::Connection object instantiated with site (with no port) specified" do
-  before(:each) do
-    @connection, @error = initialize_connection('http://192.168.0.150')
-  end
-  
-  it "should raise an error and return nil" do
+  it "should use the default CouchDB port of 5984" do
+    @connection = ActiveCouch::Connection.new('http://192.168.0.150')
+
     @connection.site.host.should == "192.168.0.150"
     @connection.site.port.should == 5984
-    @error.should == nil
   end
 end
